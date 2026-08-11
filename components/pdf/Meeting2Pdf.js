e610a5df83e96495b6e14c7969c3f3b47327edbf
Les Entreprises PEP2000 inc. <wdubreuil@pep2000.com>
2026-08-11 12:41:15 -0400

    Add files via upload

--- components/pdf/Meeting2Pdf.js
+++ components/pdf/Meeting2Pdf.js
@@ -17,7 +17,7 @@ function WeekTable({ label, dates, contremaitres, getAssignment, activeProjects,
     <View style={{ marginBottom: 12, marginTop: topGap || 0 }}>
       <View style={{ flexDirection: 'row', alignItems: 'baseline', gap: 10, marginBottom: 6 }}>
         <Text style={{ fontSize: 14, fontFamily: 'Helvetica-Bold', color: NAVY }}>{label}</Text>
-        <Text style={{ fontSize: 8, color: '#6B7280' }}>- {fmtDateLong(dates[0])} au {fmtDateLong(dates[6])}</Text>
+        <Text style={{ fontSize: 11.2, fontFamily: 'Helvetica-Bold', color: '#6B7280' }}>- {fmtDateLong(dates[0])} au {fmtDateLong(dates[6])}</Text>
       </View>
       <View style={[pdfStyles.table, { borderColor: LINE }]}>
         <View style={pdfStyles.row}>
